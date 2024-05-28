60e9053619f9796b224e4dbff820ea9b96756972
gideonzozingao <gideonzozingao@LT2308VASGIDENZ.vodafonepng.local>
2024-05-29 00:45:46 +1000

    Code Modularized

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,8 @@ from typing import Annotated
 
 from fastapi import Header, HTTPException
 
+from .database.database import SessionLocal, engine
+
 
 async def get_token_header(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret-token":
@@ -11,3 +13,11 @@ async def get_token_header(x_token: Annotated[str, Header()]):
 async def get_query_token(token: str):
     if token != "jessica":
         raise HTTPException(status_code=400, detail="No Jessica token provided")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
