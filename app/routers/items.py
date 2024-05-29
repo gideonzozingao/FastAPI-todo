05f2ad9bc079ee733a2e7cced5583145d3c2d685
gideonzozingao <gideonzozingao@LT2308VASGIDENZ.vodafonepng.local>
2024-05-30 07:49:11 +1000

    Worked on crud.py && items.py router

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, FastAPI, Response
 from sqlalchemy.orm import Session
 from ..dependencies import get_token_header, get_db
 from ..database import crud, models, schemas
@@ -11,8 +11,31 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
 
 @router.get("/", response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@router.get("/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    item = crud.get_item(db, item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+
+@router.put(
+    "/{item_id}",
+    tags=["custom"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_item(item_id: str):
+    if item_id != "plumbus":
+        raise HTTPException(
+            status_code=403, detail="You can only update the item: plumbus"
+        )
+    return {"item_id": item_id, "name": "The great Plumbus"}
